52554d2182701a5c02879a5f9c946acf28547d3a
Sandip Zalte <sandipzalte007@gmail.com>
2023-05-13 20:54:59 +0530

    Topic_Collection

---
+++ src/List_InterFace/Linked_List1.java
@@ -0,0 +1,32 @@
+package List_InterFace;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Linked_List1 {
+	public static void main(String[] args) {
+		
+	//Declaration Of arrays
+		/*ArrayList arr= new ArrayList();
+		List ar1=new ArrayList<>();
+		ArrayList<Integer> le=new ArrayList<>();
+		ArrayList<String>str=new ArrayList<>();*/
+		
+		
+		//Add()
+		ArrayList ar= new ArrayList();
+		ar.add(420);
+		ar.add('A');
+		ar.add(null);
+		ar.add("WELLCOME");
+		ar.add(null);
+		ar.add(12.45);
+		ar.add(-454.14);
+		
+		//Print the Arrays;
+		System.out.println(ar);
+		
+	}
+
+}
